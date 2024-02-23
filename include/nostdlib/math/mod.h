67ebf1ff3503cda297fb94ad5f09241d954b5a99
Ilya Akkuzin <gr3yknigh1@gmail.com>
2024-02-23 22:05:54 +0300

    Remove mod and round funcs (temporary)

--- include/nostdlib/math/mod.h
+++ include/nostdlib/math/mod.h
@@ -1,20 +1,19 @@
 #ifndef NOSTDLIB_MATH_MOD_H_
 #define NOSTDLIB_MATH_MOD_H_
 
+#include <math.h>
+
 #include "nostdlib/macros.h"
 #include "nostdlib/types.h"
 
-#define _NOC_MOD_NAME(__TYPE) noc_##__TYPE##_mod
-#define _NOC_MOD_DEF(__TYPE)                                                   \
-    __TYPE _NOC_MOD_NAME(__TYPE)(__TYPE x, __TYPE * out_ipart)
+// #define _NOC_MOD_NAME(__TYPE) noc_##__TYPE##_mod
+// #define _NOC_MOD_DEF(__TYPE) \
+//     __TYPE _NOC_MOD_NAME(__TYPE)(__TYPE x, __TYPE * out_ipart)
 
 #define NOC_MOD(__X, __OUT_IPART_PTR)                                          \
-    _Generic((__X),                                                            \
-        f32: _NOC_MOD_NAME(f32),                                               \
-        f64: _NOC_MOD_NAME(f64),                                               \
-        default: _NOC_MOD_NAME(f32))(__X, __OUT_IPART_PTR)
+    _Generic((__X), f32: modff, f64: modf, default: modf)(__X, __OUT_IPART_PTR)
 
-NOC_DEF _NOC_MOD_DEF(f32);
-NOC_DEF _NOC_MOD_DEF(f64);
+// NOC_DEF _NOC_MOD_DEF(f32);
+// NOC_DEF _NOC_MOD_DEF(f64);
 
 #endif // NOSTDLIB_MATH_MOD_H_
--- include/nostdlib/math/round.h
+++ include/nostdlib/math/round.h
@@ -4,15 +4,12 @@
 #include "nostdlib/macros.h"
 #include "nostdlib/types.h"
 
-NOC_DEF f32 noc_f32_round(f32 x);
-NOC_DEF f64 noc_f64_round(f64 x);
+// NOC_DEF f32 noc_f32_round(f32 x);
+// NOC_DEF f64 noc_f64_round(f64 x);
 
 #include <math.h>
 
 #define NOC_ROUND(__X)                                                         \
-    _Generic((__X),                                                            \
-        f32: noc_f32_round,                                                    \
-        f64: noc_f32_round,                                                    \
-        default: noc_f32_round)(__X)
+    _Generic((__X), f32: roundf, f64: round, default: roundf)(__X)
 
 #endif // NOSTDLIB_MATH_ROUND_H_
