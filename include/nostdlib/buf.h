8e46aeee71a4d2d99bc177eb8eee80956e4cbf8a
Ilya Akkuzin <gr3yknigh1@gmail.com>
2024-07-14 16:47:39 +0300

    Rename buffer module

--- include/nostdlib/buf.h
+++ include/nostdlib/buf.h
@@ -1,19 +1,16 @@
-#ifndef NOSTDLIB_BUF_H_
+#if !defined(NOSTDLIB_BUF_H_)
 #define NOSTDLIB_BUF_H_
 
 #include "nostdlib/macros.h"
 #include "nostdlib/types.h"
 
 typedef struct {
-    byte *data;
-    usize size;
-} noc_buf_t;
+    byte *Data;
+    usize Size;
+} NOC_Buffer;
 
-/*
- * Initializes empty buffer.
- * */
-NOC_DEF noc_buf_t noc_buf_init(void);
+NOC_DEF NOC_Buffer NOC_BufferMake(void);
 
-NOC_DEF noc_buf_t noc_buf_init_ex(byte *data, usize size);
+NOC_DEF NOC_Buffer NOC_BufferMakeEx(byte *data, usize size);
 
 #endif // NOSTDLIB_BUF_H_
--- src/buf.c
+++ src/buf.c
@@ -1,17 +1,17 @@
 #include "nostdlib/buf.h"
 
-NOC_DEF noc_buf_t
-noc_buf_init(void) {
-    return LITERAL(noc_buf_t){
-        .data = nullptr,
-        .size = 0,
+NOC_DEF NOC_Buffer
+NOC_BufferMake(void) {
+    return LITERAL(NOC_Buffer){
+        .Data = nullptr,
+        .Size = 0,
     };
 }
 
-NOC_DEF noc_buf_t
-noc_buf_init_ex(byte *data, usize size) {
-    return LITERAL(noc_buf_t){
-        .data = data,
-        .size = size,
+NOC_DEF NOC_Buffer
+NOC_BufferMakeEx(byte *data, usize size) {
+    return LITERAL(NOC_Buffer){
+        .Data = data,
+        .Size = size,
     };
 }
